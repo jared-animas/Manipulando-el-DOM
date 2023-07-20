a31cf87e63016f217f704ab4489e121342fd0141
jared-animas <129801788+jared-animas@users.noreply.github.com>
2023-07-20 18:04:25 -0500

    clase03 terminada

---
+++ script.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector('[data-form-btn]');
+
+const createTask = (evento) => {
+    evento.preventDefault();
+    const input = document.querySelector('[data-form-input]');
+    const value = input.value;
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    task.classList.add('card');
+    input.value = '';
+    const content = `<div>
+    <i class="far fa-check-square icon"></i>
+    <span class="task">${value}</span>
+  </div>
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
+
+    task.innerHTML = content;
+    list.appendChild(task);
+    console.log(content);
+}
+btn.addEventListener('click', createTask);
